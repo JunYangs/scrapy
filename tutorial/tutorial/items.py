aacc3522e95b56108b8e5f463a60e1ab408876bd
qiu_jy <ntesqiu@163.com>
2019-08-09 20:01:36 +0800

    获取zj政务服务行政处罚结果.

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -10,5 +10,9 @@ import scrapy
 
 class TutorialItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    caseName = scrapy.Field()  # 案件名称
+    name = scrapy.Field()  # 被处罚对象
+    documentNo = scrapy.Field()  # 行政处罚决定书文号
+    date = scrapy.Field()  # 处罚日期
+    detailUrl = scrapy.Field()  # 详细内容url
+
