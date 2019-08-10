cdbcdbe85ed7f26aeaa2789e7eef5fd6fb1a575c
qiu_jy <ntesqiu@163.com>
2019-08-10 17:58:01 +0800

    获取zj政务服务行政处罚结果.

--- tutorial/tutorial/spiders/zjgov.py
+++ tutorial/tutorial/spiders/zjgov.py
@@ -20,27 +20,28 @@ class ZjGov(scrapy.Spider):
          'http://www.zjzwfw.gov.cn/zjzw/punish/frontpunish/searchall_list.do?areacode=330101&xzcf_code=&pageNo=1'
     ]
 
-    # def start_requests(self):  # start_urls=[] 第二种写法
-    #     urls = [
-    #         'http://www.zjzwfw.gov.cn/zjzw/punish/frontpunish/searchall_list.do?areacode=330101&xzcf_code=&pageNo=1'
-    #     ]
-    #     for url in urls:
-    #         yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-        yield scrapy.Request(response.url, callback=self.parseNext)  # 第一页
+    # 页码处理
+    def parseOther(self, response):
         pageNum = response.xpath('//*[@id="xzcf_1"]/div/ul/li[6]/text()').extract()[0]
         pageNum = re.compile(r'\d+').findall(pageNum)[0]  # 总页数
-        urlSplit = self.start_urls[0].split('=', 3)  # 其它页
-        urlNext = urlSplit[0]+'='+urlSplit[1]+'='+urlSplit[2]+'='
-        print(urlNext)
-        for i in range(2, int(pageNum)+1):
-            yield scrapy.Request(urlNext+'%d' % i, callback=self.parseNext)
+        # Url拆分合并
+        urlSplit = self.start_urls[0].split('=', 3)  # 下一页
+        urlNext = urlSplit[0] + '=' + urlSplit[1] + '=' + urlSplit[2] + '='
+        list = [pageNum, urlNext]
+        return list
 
-        # for page in response.xpath('//*[@id="xzcf_1"]/div/ul/li[6]'):  # 其它页
-        #     urlNext = page.xpath('').extract()[0]
-        #     yield scrapy.Request(urlNext, callback=self.parseNext)
+    # 页面解析
+    def parse(self, response):
+        yield scrapy.Request(response.url, callback=self.parseNext)  # 第一页
+        # pageNum = self.parseOther(response)[0]
+        # urlNext = self.parseOther(response)[1]
+        flag = True
+        while flag:
+            for i in range(2, int(self.parseOther(response)[0])+1):
+                yield scrapy.Request(self.parseOther(response)[1]+str(i), callback=self.parseNext)
+            flag = False
 
+    # 页面信息处理
     def parseNext(self, response):
         for i in response.xpath('//*[@id="xzcf_1"]/table'):
             item = TutorialItem()
