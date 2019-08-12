94f0a609abc640714a5905b1c1fba40c8537295b
qiu_jy <ntesqiu@163.com>
2019-08-12 21:07:43 +0800

    各市首URL

---
+++ tutorial/tutorial/ISConstant.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# 浙江省各市首链接
+cityUrl = {
+    'hangzhou': 'http://www.zjzwfw.gov.cn/zjzw/punish/frontpunish/searchall_list.do?areacode=330101&xzcf_code=&pageNo=1',
+    'wenzhou': 'http://www.zjzwfw.gov.cn/zjzw/punish/frontpunish/searchall_list.do?areacode=330301&xzcf_code=&pageNo=1'
+}
