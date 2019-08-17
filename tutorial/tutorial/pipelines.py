159e6a98fa1aa0de0545edf24a1d29c0c6228cfa
qiu_jy <ntesqiu@163.com>
2019-08-17 17:52:42 +0800

    全省数据

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -4,8 +4,12 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
 
 
 class TutorialPipeline(object):
+    def __init__(self):
+        self.conn = pymysql.connect("localhost", "root", "root", "")
+
     def process_item(self, item, spider):
         return item
