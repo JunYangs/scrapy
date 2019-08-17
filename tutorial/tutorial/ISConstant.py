7bc7b9be51d3c8c3b435560c206d50be03889e85
qiu_jy <ntesqiu@163.com>
2019-08-17 17:52:18 +0800

    全省数据

--- tutorial/tutorial/ISConstant.py
+++ tutorial/tutorial/ISConstant.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+# 浙江政务服务网域名
+SITEURL = 'http://www.zjzwfw.gov.cn'
+
 # 浙江省各市首链接
 cityUrl = {
     'hangzhou': 'http://www.zjzwfw.gov.cn/zjzw/punish/frontpunish/searchall_list.do?areacode=330101&xzcf_code=&pageNo=1',
     'wenzhou': 'http://www.zjzwfw.gov.cn/zjzw/punish/frontpunish/searchall_list.do?areacode=330301&xzcf_code=&pageNo=1'
+}
+
+# URL头部
+HEADERS = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
 }
