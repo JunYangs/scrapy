9cb5c08691ba7f12ddd0ac3521b75623d96c5b3b
qiu_jy <ntesqiu@163.com>
2019-08-20 22:38:18 +0800

    存量增量

--- tutorial/tutorial/ISConstant.py
+++ tutorial/tutorial/ISConstant.py
@@ -4,13 +4,14 @@
 # 浙江政务服务网域名
 SITEURL = 'http://www.zjzwfw.gov.cn'
 
-# 浙江省各市首链接
+# 添加浙江省各市URL
 cityUrl = {
     'hangzhou': 'http://www.zjzwfw.gov.cn/zjzw/punish/frontpunish/searchall_list.do?areacode=330101&xzcf_code=&pageNo=1',
     'wenzhou': 'http://www.zjzwfw.gov.cn/zjzw/punish/frontpunish/searchall_list.do?areacode=330301&xzcf_code=&pageNo=1'
 }
 
-# URL头部
+# 浏览器标识
 HEADERS = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                'Chrome/73.0.3683.86 Safari/537.36'
 }
