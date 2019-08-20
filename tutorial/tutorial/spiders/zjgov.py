2987493f61edad59996ff5e33d70b78a352517de
qiu_jy <ntesqiu@163.com>
2019-08-20 22:37:40 +0800

    存量增量

--- tutorial/tutorial/spiders/zjgov.py
+++ tutorial/tutorial/spiders/zjgov.py
@@ -9,6 +9,7 @@
 '''
 
 import re
+import time
 import scrapy
 import requests
 from lxml import etree
@@ -22,6 +23,12 @@ class ZjGov(scrapy.Spider):
     start_urls = [
          'http://www.zjzwfw.gov.cn/zjzw/punish/frontpunish/searchall_list.do?areacode=330101&xzcf_code=&pageNo=1'
     ]
+    incrementList = []  # 增量非增量对比
+    YN = False  # 增量获取是否结束
+
+    def __init__(self, category=None, *args, **kwargs):
+        super(ZjGov, self).__init__(*args, **kwargs)
+        self.increment = category  # 是否增量
 
     # URL拼接
     def parseOther(self, response, cityUrl):
@@ -33,14 +40,18 @@ class ZjGov(scrapy.Spider):
         list = [pageNum, urlNext]
         return list
 
-    # 页面获取
+    # 页面获取(增量或存量模式)
     def parse(self, response):
         for cityUrl in list(tutorial.ISConstant.cityUrl.values()):
             yield scrapy.Request(cityUrl, callback=self.parseNext)
+            self.YN = False
+            self.incrementList.clear()
             pageNum = self.parseOther(response, cityUrl)[0]  # 总页数
             urlNext = self.parseOther(response, cityUrl)[1]  # 下一页
             for i in range(2, int(pageNum)+1):
                 yield scrapy.Request(urlNext+str(i), callback=self.parseNext)
+                if self.increment and self.YN is True:  # 增量且获取结束
+                    break
 
     # 页面解析
     def parseNext(self, response):
@@ -53,6 +64,10 @@ class ZjGov(scrapy.Spider):
             item['date'] = i.xpath('tr/td[4]/text()').extract()[0]  # 处罚日期
             urltail = i.xpath('tr/td[1]/a/@href').extract()[0]
             item['detail'] = self.parseContent(urlHead+urltail)  # 行政处罚详细内容
+            if self.increment:  # 增量数据对比
+                self.inComp(item['date'])
+                if self.YN is True:
+                    break
             yield item
 
     # 行政处罚详细内容
@@ -61,3 +76,14 @@ class ZjGov(scrapy.Spider):
         html = etree.HTML(res)
         content = html.xpath('/html/body/table[3]/tr/td/table/tr/td/table[5]/tr/td/p/text()')
         return content
+
+    # 增量数据对比
+    def inComp(self, date):
+        localTime = time.strftime("%Y-%m-%d", time.localtime())
+        list = self.incrementList
+        list.append(date)
+        if(len(list)-list.count(localTime)) >= 50:
+            self.YN = True
+
+
+
